7e5ca268affa401845e194a8b99148976a097b9d
Hristiyan Petrov <hristiyan_petrov@abv.bg>
2024-10-20 19:48:29 +0300

    Style header

--- first-nextjs-project/my-app/app/layout.js
+++ first-nextjs-project/my-app/app/layout.js
@@ -1,3 +1,4 @@
+import { Fugaz_One } from "next/font/google"
 import localFont from "next/font/local";
 import "./globals.css";
 import Main from "@/components/Main";
@@ -13,22 +14,35 @@ const geistMono = localFont({
 	weight: "100 900",
 });
 
+const fugaz = Fugaz_One({ subsets: ["latin"], weight: '400' });
+
 export const metadata = {
 	title: "Broodl",
 	description: "Track your daily mood.",
 };
 
 export default function RootLayout({ children }) {
+	const header = (
+		<header className="p-4 sm:p-8 flex items-center justify-between gap-4">
+			<h1 className={'text-base sm:text-lg textgradient ' + fugaz.className}>Broodl</h1>
+		</header>
+	);
+
+	const footer = (
+		<footer>Footer</footer>
+	);
+
 	return (
 		<html lang="en">
 			<body
-			// className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+				className={`w-full max-w-[1000px] mx-auto text-sm 
+					sm:text-base min-h-screen flex-col ` + `${geistSans.variable} ${geistMono.variable} antialiased`}
 			>
-				<header>Header</header>
+				{header}
 				<Main>
 					{children}
 				</Main>
-				<footer>Footer@</footer>
+				{footer}
 			</body>
 		</html>
 	);
