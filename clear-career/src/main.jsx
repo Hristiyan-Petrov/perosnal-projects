fbf6241e8f8736ece708c1e2607f612923716288
Hristiyan Petrov <hristiyan_petrov@abv.bg>
2024-10-23 19:41:07 +0300

    Initialize react-router-dom and add error page

--- clear-career/src/main.jsx
+++ clear-career/src/main.jsx
@@ -1,14 +1,23 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
 import './index.css'
 import App from './App.jsx'
+import Root from './routes/root.jsx'
+import ErrorPage from './routes/error-page.jsx'
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Root />,
+        errorElement: <ErrorPage />
+        // element: <div>Hello World!</div>
+    }
+]);
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
+        <RouterProvider router={router} />
     </StrictMode>,
 )
