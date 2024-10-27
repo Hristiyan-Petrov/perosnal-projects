0860762876e0b7195db6bfbe89762255ff3f73e0
Hristiyan Petrov <hristiyan_petrov@abv.bg>
2024-10-27 22:48:18 +0200

    Add create contacs and get all

--- react-router-dom-tutorial/src/main.jsx
+++ react-router-dom-tutorial/src/main.jsx
@@ -2,28 +2,28 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
-import './index.css'
-import App from './App.jsx'
-import Root from './routes/root.jsx'
+import './indexClaude.css'
+import Root, {
+    loader as rootLoader,
+    action as rootAction,
+} from './routes/root.jsx'
 import ErrorPage from './routes/error-page.jsx'
-import Contact from './routes/contacts.jsx'
+import Contact from './routes/contact.jsx'
 
 const router = createBrowserRouter([
     {
         path: '/',
         element: <Root />,
         errorElement: <ErrorPage />,
+        loader: rootLoader,
+        action: rootAction,
         children: [
             {
-                path: 'contacts/:contactId',
+                path: '/contacts/:contactId',
                 element: <Contact />
             }
         ]
-    }, 
-    {
-        path: 'contacts/:contactId',
-        element: <Contact />
-    }
+    },
 ]);
 
 createRoot(document.getElementById('root')).render(
